2d6610399799f81a7abafbb2f5a6dbbea8ad2773
matth <mattheew.oliveira@gmail.com>
2023-10-25 11:52:38 -0300

    Create TodoService

--- src/main/java/br/com/matheus/desafiotodolist/service/TodoService.java
+++ src/main/java/br/com/matheus/desafiotodolist/service/TodoService.java
@@ -1,2 +1,36 @@
-package br.com.matheus.desafiotodolist.service;public class TodoService {
+package br.com.matheus.desafiotodolist.service;
+
+import br.com.matheus.desafiotodolist.entity.Todo;
+import br.com.matheus.desafiotodolist.repository.TodoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TodoService {
+
+    private TodoRepository repository;
+    public List<Todo> create(Todo todo) {
+      this.repository.save(todo);
+      return list();
+    }   public List<Todo> list() {
+      Sort sort  = Sort.by("prioridade")
+              .descending().and(Sort.by("nome")
+                      .ascending());
+        return this.repository.findAll(sort);
+
+    }   public List<Todo> update(Todo todo) {
+        repository.save(todo);
+   return list();
+    }   public List<Todo> delete(Long id) {
+        repository.deleteById(id);
+        return list();
+
+    }
 }
