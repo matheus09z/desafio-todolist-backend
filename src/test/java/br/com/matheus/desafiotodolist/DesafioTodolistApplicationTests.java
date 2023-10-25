be9461b40235287b049d30aca88174d0fd44fa74
matth <mattheew.oliveira@gmail.com>
2023-10-25 11:17:38 -0300

    initial commit

---
+++ src/test/java/br/com/matheus/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
