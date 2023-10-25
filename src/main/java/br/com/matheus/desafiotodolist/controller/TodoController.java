8e079b9ca4c32dfb582207c98cac83fe808a27c6
matth <mattheew.oliveira@gmail.com>
2023-10-25 11:54:18 -0300

    Create TodoController

--- src/main/java/br/com/matheus/desafiotodolist/controller/TodoController.java
+++ src/main/java/br/com/matheus/desafiotodolist/controller/TodoController.java
@@ -1,2 +1,40 @@
-package br.com.matheus.desafiotodolist.controller;public class TodoController {
+package br.com.matheus.desafiotodolist.controller;
+
+
+import br.com.matheus.desafiotodolist.entity.Todo;
+import br.com.matheus.desafiotodolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+
+    }
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+
+    }
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+
+    }
+    @DeleteMapping("/{id}")
+    List<Todo> delete(@PathVariable Long id) {
+        return todoService.delete(id);
+
+    }
 }
