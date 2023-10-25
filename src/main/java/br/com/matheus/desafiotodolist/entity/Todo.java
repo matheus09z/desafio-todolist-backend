d1f4acaeae04d45933ea4abbef9b7cf15e489b49
matth <mattheew.oliveira@gmail.com>
2023-10-25 12:48:06 -0300

    Colocando a anotação NotBlank

--- src/main/java/br/com/matheus/desafiotodolist/entity/Todo.java
+++ src/main/java/br/com/matheus/desafiotodolist/entity/Todo.java
@@ -1,6 +1,7 @@
 package br.com.matheus.desafiotodolist.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -13,9 +14,19 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public  Todo(String nome, String descricao, boolean realizado,
+                 int prioridade){
+        this.descricao = descricao;
+        this.nome = nome;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 
 }
